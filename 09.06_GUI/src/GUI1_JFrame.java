a9b73b95a48af6c72eeb3ebd2ec5558407551b1a
KimSujin02 <s2018w28@e-mirim.hs.kr>
2019-09-06 09:07:47 +0900

    어쩌다 윈도우 Label

--- 09.06_GUI/src/GUI1_JFrame.java
+++ 09.06_GUI/src/GUI1_JFrame.java
@@ -3,6 +3,10 @@ import javax.swing.*;
 public class GUI1_JFrame {
     public static void main(String args[]) {
         JFrame frame = new JFrame("GUI 창이름");
+        JLabel label = new JLabel("아싸 어쩌다 금요일"); //ctl + shift + enter 하면 끝에 세미클로이 입력된다.
+
+        frame.add(label);
+
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //x버튼 -> 끝
