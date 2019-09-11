f4ea6c04e953778554c0e1f1209d7ff1fefaf4dc
KimSujin02 <s2018w28@e-mirim.hs.kr>
2019-09-11 10:07:27 +0900

    Components, label, button, checkbox, radiobutton + buttongroup, textfield, textarea, combobox

---
+++ 09.06_GUI/src/GUI2_Component.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI2_Component {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Conponents");
+        JPanel panel = new JPanel();
+        JLabel lb0 = new JLabel("JLabel");
+        JButton bt0 = new JButton("JButton");
+        JCheckBox cb0 = new JCheckBox("JCheckBox0");
+        JCheckBox cb1 = new JCheckBox("JCheckBox1");
+
+        JRadioButton rb0 = new JRadioButton("미성년자");
+        JRadioButton rb1 = new JRadioButton("성인");
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(rb0);
+        bg.add(rb1);
+
+        JTextField tf0 = new JTextField("JTextField0");
+        JTextArea ta0 = new JTextArea(2, 10); //문구를 넣을 수 있는 영역, 크기를 조정 할 수 있음
+
+        String[] EXO = {"한명을 선택해주세요", "도경수", "박찬열", "김민석", "김준면", "오세훈", "김종대", "장이씽", "김종인", "변백현"};
+        JComboBox<String> cbox0 = new JComboBox<>(EXO);
+        cbox0.addItem("김수진"); //이렇게 나중에 추가하는 방법도 있다.
+
+        panel.add(lb0);
+        panel.add(bt0);
+        panel.add(cb0);
+        panel.add(cb1);
+        panel.add(rb0);
+        panel.add(rb1);
+        panel.add(tf0);
+        panel.add(ta0);
+        panel.add(cbox0);
+
+        frame.add(panel);
+
+        frame.setPreferredSize(new Dimension(600, 400));
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
