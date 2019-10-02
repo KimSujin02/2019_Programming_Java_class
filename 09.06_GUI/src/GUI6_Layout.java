ab758179eb5d14c4e552c87c73ffcd050e246c02
KimSujin02 <s2018w28@e-mirim.hs.kr>
2019-10-02 09:53:07 +0900

    패널 안의 패널

--- 09.06_GUI/src/GUI6_Layout.java
+++ 09.06_GUI/src/GUI6_Layout.java
@@ -3,15 +3,18 @@ import java.awt.*;
 
 public class GUI6_Layout {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("GUI화면 임시로 띄어놓음");
+        JFrame frame = new JFrame();
         JPanel panel = new JPanel();
 
-        JLabel lbTitle  =  new JLabel("아 마 스 빈");
+        BorderLayout bl=new BorderLayout();
+        panel.setLayout(bl);
+
+        JLabel lbTitle = new JLabel(" 아 마 스 빈 ");
         lbTitle.setHorizontalAlignment(JLabel.CENTER);
-        JTextArea taList = new JTextArea(1, 30);
+        JTextArea taList=new JTextArea(1,30);
         JPanel eastPanel = new JPanel();
         int sum = 10000;
-        JLabel lbSum  = new JLabel("총 금액 : " + sum);
+        JLabel lbSum=new JLabel("총 금액: "+sum);
         JButton btPayment = new JButton("결제하기");
 
         eastPanel.add(lbSum);
@@ -22,7 +25,7 @@ public class GUI6_Layout {
 
         frame.add(panel);
 
-        frame.setPreferredSize(new Dimension(600, 400));
+        frame.setPreferredSize(new Dimension(600,400));
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
